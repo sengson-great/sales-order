47fffab9a1b4f53c6c53deb4dabe7d21ecd27c56
Seng Vengchhuong <leobomo5@gmail.com>
2026-02-04 16:01:51 +0700

    hmm

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -9,6 +9,7 @@ import {
 } from "react";
 import api from "@/api/api";
 import { useRouter } from "next/navigation";
+import { useSalesAuth } from "./SalesAuthContext";
 
 interface User {
   id: number;
@@ -43,6 +44,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const refreshTimeoutRef = useRef<NodeJS.Timeout | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const { salesLogout } = useSalesAuth();
 
   // 🔹 Unified function to extract user data
   const extractUserFromResponse = (responseData: any): User | null => {
