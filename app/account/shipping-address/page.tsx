78b35134f7e11f5ed3e0a6dc36811aa62f2b66d0
Seng Vengchhuong <leobomo5@gmail.com>
2026-02-08 12:03:08 +0700

    hmm

--- app/account/shipping-address/page.tsx
+++ app/account/shipping-address/page.tsx
@@ -548,12 +548,6 @@ export default function ShippingAddressPage() {
                     <p className="text-gray-600 text-sm mt-1">
                       {t.phone}: {addr.phone}
                     </p>
-                    {/* Don't show coordinates for salesOnField */}
-                    {!isSalesOnField && addr.coordinates && (
-                      <p className="text-gray-500 text-xs mt-1">
-                        📍 Lat: {Number(addr.coordinates.lat).toFixed(5)}, Lng: {Number(addr.coordinates.lng).toFixed(5)}
-                      </p>
-                    )}
                   </div>
                   
                   <div className="flex gap-2">
